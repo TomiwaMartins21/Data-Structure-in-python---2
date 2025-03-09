ff69dccd44389e701400b753c65b567cca1ed8f9
TomiwaMartins21 <martinstomiwa838@gmail.com>
2025-03-09 12:38:50 +0000

    Inital commit

---
+++ Activity-4/main.py
@@ -0,0 +1,8 @@
+setx = {"green", "blue"}
+sety = {"blue", "yellow"}
+print("Original set elements:")
+print(setx)
+print(sety)
+print("\nTntersection of two said set:")
+setz = setx.intersection(sety)
+print(setz)
